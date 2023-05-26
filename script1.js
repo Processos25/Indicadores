bb612408925e0df855526accb70d640cb9d872e6
processos <processos.salsarommanel@gmail.com>
2023-05-26 10:00:28 -0300

    indicadores.commit

---
+++ script1.js
@@ -0,0 +1,149 @@
+function verificarLogin() {
+    var inputSenha = document.getElementById('inputSenha').value;
+    var Glauce = "123";
+    var Priscila = "1234";
+    var Alexsandra = "12345";
+    var Conceicao= "123456";
+    var Edigenilma= "1234567";
+    var GabrielaMaria= "GabrielaMaria";
+    var GabrielleGomes= "GabrielleGomes";
+    var Hortencia= "Hortencia";
+    var Ovane= "Ovane";
+    var VictoriaEstefany= "VictoriaEstefany";
+    var FernandaPessoa= "FernandaPessoa";
+    var Lucivania = "Lucivania";
+    var RenataBispo= "RenataBispo";
+    var VanessaCristina= "VanessaCristina";
+    var DanielGomes= "DanielGomes";
+    var ElizaBeth = "ElizaBeth";
+    var EwertonIgor = "EwertonIgor";
+    var Larissa = "Larissa";
+    var MayraNayara= "MayraNayara";
+    var Rafaely= "Rafaely";
+    var Alana = "Alana";
+    var Anndreyna = "Anndreyna";
+    var IlariaVitoria = "IlariaVitoria";
+    var Sara= "Sara";
+    var AnnaKaroliny= "AnnaKaroliny";
+    var AnneCristine = "AnneCristine";
+    var Giselle= "Giselle";
+    var HeloisaLopes = "HeloisaLopes";
+    var Ivana= "Ivana";
+    var Wellington= "Wellington";
+    var Glaucione= "Glaucione";
+    var GabrielaRocha = "GabrielaRocha";
+    var Karla= "Karla";
+    var Lucia= "Lucia";
+    var Margareth= "Margareth";
+    var Rafaela= "Rafaela";
+    var Anakelly= "Anakelly";
+    var CesarAugusto= "CesarAugusto";
+    var camille = "camille";
+    var GabrielaAlmeida= "GabrielaAlmeida";
+    var Hildon= "Hildon";
+    var YasminMelo= "YasminMelo";
+    var Clara = "Clara";
+    var Damiana= "Damiana";
+    var DeboraFelipe= "DeboraFelipe";
+    var Alana= "Alana";
+
+
+    if (inputSenha === Glauce) {
+      window.location.href = 'https://www.pontotel.com.br/wp-content/uploads/2022/05/imagem-corporativa.jpg';
+    } else if (inputSenha === Priscila) {
+      window.location.href = 'https://img.freepik.com/fotos-gratis/terra-e-galaxia-elementos-desta-imagem-fornecidos-pela-nasa_335224-750.jpg?w=2000';
+    } else if (inputSenha === Alexsandra) {
+      window.location.href = 'https://mundoconectado.com.br/uploads/2022/05/25/25658/cacto.jpg';
+    } else if (inputSenha === Conceicao) {
+      window.location.href = 'https://img.freepik.com/fotos-gratis/imagem-aproximada-da-cabeca-de-um-lindo-leao_181624-35855.jpg?w=2000';
+    } else if (inputSenha === Edigenilma) {
+      window.location.href = 'https://ufsb.edu.br/proplan/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image04_grd.png';
+    } else if (inputSenha === GabrielaMaria) {
+      window.location.href = '#';
+    } else if (inputSenha === GabrielleGomes) {
+      window.location.href = '#';
+    } else if (inputSenha === Hortencia) {
+      window.location.href = '#';
+    } else if (inputSenha === Ovane) {
+      window.location.href = '#';
+    } else if (inputSenha === VictoriaEstefany) {
+      window.location.href = '#';
+    } else if (inputSenha === FernandaPessoa) {
+      window.location.href = '#';
+    } else if (inputSenha === Lucivania) {
+      window.location.href = '#';
+    } else if (inputSenha === RenataBispo) {
+      window.location.href = '#';
+    } else if (inputSenha === VanessaCristina) {
+      window.location.href = '#';
+    } else if (inputSenha === DanielGomes) {
+      window.location.href = '#';
+    } else if (inputSenha === ElizaBeth) {
+      window.location.href = '#';
+    } else if (inputSenha === EwertonIgor) {
+      window.location.href = '#';
+    } else if (inputSenha === Larissa) {
+      window.location.href = '#';
+    } else if (inputSenha === MayraNayara) {
+      window.location.href = '#';
+    } else if (inputSenha === Rafaely) {
+      window.location.href = '#';
+    } else if (inputSenha === Alana) {
+      window.location.href = '#';
+    } else if (inputSenha === Anndreyna) {
+      window.location.href = '#';
+    } else if (inputSenha === IlariaVitoria) {
+      window.location.href = '#';
+    } else if (inputSenha === Sara) {
+      window.location.href = '#';
+    } else if (inputSenha === AnnaKaroliny) {
+      window.location.href = '#';
+    } else if (inputSenha === AnneCristine) {
+      window.location.href = '#';
+    } else if (inputSenha === Giselle) {
+      window.location.href = '#';
+    } else if (inputSenha === HeloisaLopes) {
+      window.location.href = '#';
+    } else if (inputSenha === Ivana) {
+      window.location.href = '#';
+    } else if (inputSenha === Wellington) {
+      window.location.href = '#';
+    } else if (inputSenha === Priscila) {
+      window.location.href = '#';
+    } else if (inputSenha === Glaucione) {
+      window.location.href = '#';
+    } else if (inputSenha === GabrielaRocha) {
+      window.location.href = '#';
+    } else if (inputSenha === Karla) {
+      window.location.href = '#';
+    } else if (inputSenha === Lucia) {
+      window.location.href = '#';
+    } else if (inputSenha === Margareth) {
+      window.location.href = '#';
+    } else if (inputSenha === Rafaela) {
+      window.location.href = '#';
+    } else if (inputSenha === Anakelly) {
+      window.location.href = '#';
+    } else if (inputSenha === CesarAugusto) {
+      window.location.href = '#';
+    } else if (inputSenha === camille) {
+      window.location.href = '#';
+    } else if (inputSenha === GabrielaAlmeida) {
+      window.location.href = '#';
+    } else if (inputSenha === Hildon) {
+      window.location.href = '#';
+    } else if (inputSenha === YasminMelo) {
+      window.location.href = '#';
+    } else if (inputSenha === Clara) {
+      window.location.href = '#';
+    } else if (inputSenha === Damiana) {
+      window.location.href = '#';
+    } else if (inputSenha === DeboraFelipe) {
+      window.location.href = '#';
+    } else if (inputSenha === Alana) {
+      window.location.href = '#';
+    } 
+    else {
+      alert('Senha incorreta. Tente novamente.');
+    }
+  }  
